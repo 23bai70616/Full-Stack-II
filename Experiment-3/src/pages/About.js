1d4ae7fcaae3ecd675cd1c26af06d4f1c2ef6305
Adarsh <23BAI70616@cuchd.in>
2026-02-13 21:04:51 +0530

    experiment-3

---
+++ Experiment-3/src/pages/About.js
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@mui/material";
+
+function About() {
+  return (
+    <Container sx={{ mt: 5 }}>
+      <Typography variant="h4">About Page</Typography>
+      <Typography>React Router Navigation</Typography>
+    </Container>
+  );
+}
+
+export default About;
---
+++ Experiment-3/src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@mui/material";
+
+function Home() {
+  return (
+    <Container sx={{ mt: 5 }}>
+      <Typography variant="h4">Home Page</Typography>
+      <Typography>Experiment 3 Home Page</Typography>
+    </Container>
+  );
+}
+
+export default Home;
